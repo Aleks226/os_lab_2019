5e993653c0cab50292a51e40ed6fa7ce4a3aae65
Aleks226 <83003755+Aleks226@users.noreply.github.com>
2023-09-22 14:31:20 +0000

    добавлен empty и newhello.c

--- lab0/script.sh
+++ lab0/script.sh
@@ -2,6 +2,7 @@ clear
 cd lab0
 
 # 2
+echo "Задание №2"
 mkdir hello &> /dev/null
 cd hello
 touch empty
@@ -9,12 +10,15 @@ cp ../src/hello.c hello.c
 mv hello.c newhello.c
 
 # 3
+echo "Задание №3"
 sh ../../update.sh
 
 # 4
+echo "Задание №4"
 gcc newhello.c -o hello_world
 
 # end
+echo "Конец"
 cd ..
 git add ./hello/empty
 git add ./hello/newhello.c
