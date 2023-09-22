d7023e231036afdd59799a617453513981afdefb
Aleks226 <83003755+Aleks226@users.noreply.github.com>
2023-09-22 13:51:19 +0000

    добавлен average.sh и generate.sh

--- lab1/script.sh
+++ lab1/script.sh
@@ -1,26 +1,31 @@
 clear
 cd lab1
+
 # 1
 cd src
-#./background.sh &
+./background.sh &
 touch empty # написал в файле empty "Something in file! ;)"
 cat empty
 # clear
 wc empty -c
+
 # 2
 grep "cake" cake_rhymes.txt > with_cake.txt
 rm file.txt &> /dev/null
+
 # 3
 chmod u+x ./hello.sh
 ./hello.sh
 touch path.sh # написал скрипт
 ./path.sh
+
 # 4
 touch average.sh # написал скрипт
 touch numbers.txt
 touch generate.sh # написал скрипт
 ./generate.sh
 ./average.sh < ./numbers.txt
+
 # end
 cd ..
 git add ./src/empty
