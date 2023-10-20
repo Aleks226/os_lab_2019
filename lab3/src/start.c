e83aa8b07d4a22306a4580bb480a022d78c1e01e
Aleks226 <83003755+Aleks226@users.noreply.github.com>
2023-10-20 14:02:46 +0000

    изменены find_min_max.c, parallel_min_max.c, start.c и makefile

--- lab3/src/start.c
+++ lab3/src/start.c
@@ -5,7 +5,7 @@
 int main(int argc, char **argv){
     int pid = fork();
 	if ( pid == 0 ) {
-		execv( "./sequential_min_max.c", argv);
+		execv( "./sequential_min_max", argv);
 	}
 	/* Put the parent to sleep for 2 seconds--let the child finished executing */
     return 0;
