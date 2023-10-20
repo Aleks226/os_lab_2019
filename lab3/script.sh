c7f86a2b2a4f0eaf6e2f2a24eb1536b0e5f16f18
Aleks226 <83003755+Aleks226@users.noreply.github.com>
2023-10-20 13:21:47 +0000

    изменены find_min_max.c, parallel_min_max.c, start.c и makefile

--- lab3/script.sh
+++ lab3/script.sh
@@ -4,7 +4,7 @@ cd lab3/src
 # 1
 echo "Задание №1"
 gcc -o seq sequential_min_max.c find_min_max.c utils.c
-./seq --seed "2" --array_size "8" --pnum "4" -f
+./seq --seed "2" --arraysize "8"
 
 # end
 echo "Конец"
