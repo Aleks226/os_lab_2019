e38e5430fc30df44f005cf623b63a8eb636e3a74
Aleks226 <83003755+Aleks226@users.noreply.github.com>
2023-12-01 13:56:30 +0000

    изменены find_min_max.c, parallel_min_max.c, start.c и makefile

--- lab3/src/parallel_min_max.c
+++ lab3/src/parallel_min_max.c
@@ -5,7 +5,6 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <signal.h>     // 1
 
 #include <sys/time.h>
 #include <sys/types.h>
@@ -13,31 +12,31 @@
 
 #include <getopt.h>
 
+#include "find_sum.h"
 #include "find_min_max.h"
 #include "utils.h"
 
-void KILLCHILD (pid_t id) {     // 6
-  kill(id, SIGKILL);
-  printf("Hello I am die");
-  int status;
-  wait(&status);
-}
+
+void kill_child(pid_t id);
 
 int main(int argc, char **argv) {
   int seed = -1;
   int array_size = -1;
   int pnum = -1;
+  int timeout = -1;
   bool with_files = false;
-  int time = 0;     // 1
 
   while (true) {
+   
+
+  //обработка входных аргументов
     int current_optind = optind ? optind : 1;
 
     static struct option options[] = {{"seed", required_argument, 0, 0},
                                       {"array_size", required_argument, 0, 0},
                                       {"pnum", required_argument, 0, 0},
                                       {"by_files", no_argument, 0, 'f'},
-                                      {"time", required_argument, 0, 0},          // 1
+                                      {"timeout", required_argument, 0, 0},
                                       {0, 0, 0, 0}};
 
     int option_index = 0;
@@ -50,43 +49,35 @@ int main(int argc, char **argv) {
         switch (option_index) {
           case 0:
             seed = atoi(optarg);
-            // your code here
-            // error handling
-            if (seed <= 0) {              // 4
-              printf("seed is a positive number\n");
+           if (seed <= 0) {
+              printf("seed must be a positive number\n");
               return 1;
             }
             break;
           case 1:
             array_size = atoi(optarg);
-            // your code here
-            // error handling
-            if (array_size <= 0) {            // 4
-              printf("array_size is a positive number\n");
+             if (array_size <= 0) {
+              printf("array_size must be a positive number\n");
               return 1;
             }
             break;
           case 2:
             pnum = atoi(optarg);
-            // your code here
-            // error handling
-            if (pnum <= 0) {            // 4
-              printf("pnum is a positive number\n");
+             if (pnum <= 0) {
+              printf("pnum must be a positive number\n");
               return 1;
             }
             break;
           case 3:
             with_files = true;
             break;
-          case 4:                 // 8  
-            time = atoi(optarg);
-            // error handling
-            if (time <= 0) {
-              printf("pnum is a positive number\n");
+          case 4:
+            timeout = atoi(optarg);
+            if (timeout <= 0) {
+              printf("timeout must be a positive number\n");
               return 1;
             }
             break;
-
           defalut:
             printf("Index %d is out of options\n", option_index);
         }
@@ -108,15 +99,13 @@ int main(int argc, char **argv) {
     return 1;
   }
 
-  if (seed == -1 || array_size == -1 || pnum == -1) {
+  if (seed == -1 || array_size == -1 || pnum == -1 || timeout == -1) {
     printf("Usage: %s --seed \"num\" --array_size \"num\" --pnum \"num\" \n",
            argv[0]);
     return 1;
   }
 
-  alarm(time);
-  sleep(time/2);
-
+//инициализация массива и данных
   int *array = malloc(sizeof(int) * array_size);
   GenerateArray(array, array_size, seed);
   int active_child_processes = 0;
@@ -124,79 +113,102 @@ int main(int argc, char **argv) {
   struct timeval start_time;
   gettimeofday(&start_time, NULL);
 
-  FILE *fp;  // 2
-  fp=fopen("test.txt", "w");
 
-  int pipefd[2];  // 4
-  pipe(pipefd);
+//заводим будильник
+alarm(timeout);
+sleep(timeout/2);
+
+
+//подготовка к синхронизации
+  int proc_elements_num = array_size / pnum; // количество элементов которые будет обрабатывать каждый процесс
   pid_t kill_pid = -1;
-  int done_flag = 0;
 
+  FILE *file;
+  if(with_files) {
+    file = fopen("sync_file.txt", "w");
+  }
+
+  int pipefd[2];
+  if(!with_files){
+    pipe(pipefd);
+  }
+
+
+//распараллеливаем
+
+  int start_ind, end_ind;
+  
   for (int i = 0; i < pnum; i++) {
     pid_t child_pid = fork();
     if (child_pid >= 0) {
       // successful fork
       active_child_processes += 1;
+      //prinft("make %d child",active_child_processes);
       if (child_pid == 0) {
         // child process
-        // parallel somehow
-        struct MinMax min_max = GetMinMax(array, (array_size*i)/pnum, (array_size*(i+1))/pnum);        // 1
+        start_ind = i * proc_elements_num;
+        if(i == pnum - 1) end_ind = array_size;
+        else end_ind = (i+1)*proc_elements_num;
+
+        struct MinMax min_max = GetMinMax(array, start_ind, end_ind);
+
+// синхронизация
+  //с использованием файлов
         if (with_files) {
-          // use files here	
-          fprintf(fp, "%d %d\n", min_max.max,min_max.min);          // 1
-        } else {
-          // use pipe here
-          close(pipefd[0]);          // 4
-          write(pipefd[1], &min_max.max, sizeof(min_max.max));
-          write(pipefd[1], &min_max.min, sizeof(min_max.min));
-          //close(pipefd[1]);
+          fprintf(file, "%d %d\n",min_max.min, min_max.max);
+        } 
+  //с использованием pipe
+        else {
+          write(pipefd[1], &min_max.min, sizeof(int));
+          write(pipefd[1], &min_max.max, sizeof(int));
+          close(pipefd[0]);
         }
         return 0;
       }
-      else {    // 4
+      else {
         kill_pid = child_pid;
-        signal(SIGALRM, KILLCHILD);
+        signal(SIGALRM, kill_child);
       }
-
-    } else {
+    } 
+    else {
       printf("Fork failed!\n");
       return 1;
     }
   }
-  fclose(fp);  // 1
+
+if(with_files) fclose(file);
+
+//убиваем детей
   while (active_child_processes > 0) {
-    // your code here
-    //wait(NULL);    // 2
-    signal (SIGALRM, KILLCHILD);
+    //printf("amount of kids: %d\n", active_child_processes);
+    signal (SIGALRM, kill_child);
     active_child_processes -= 1;
   }
 
+
+//находим конечный минимум и максимум
+  if(with_files) file = fopen("sync_file.txt", "r");
   struct MinMax min_max;
   min_max.min = INT_MAX;
   min_max.max = INT_MIN;
 
-  fp=fopen("test.txt", "r");  // 1
   for (int i = 0; i < pnum; i++) {
     int min = INT_MAX;
     int max = INT_MIN;
 
     if (with_files) {
-      // read from files
-      fscanf(fp,"%d %d",&max,&min);      // 2
-      printf("Max_read: %d Min_read: %d\n", max, min);;
+      //read from file
+      fscanf(file,"%d %d", &min, &max);
     } else {
       // read from pipes
-      read(pipefd[0], &max, 4);      // 4
-      read(pipefd[0], &min, 4);
-      printf("Max_read: %d Min_read: %d\n", max, min);
-      //close(pipefd[0]);
-
+      read(pipefd[0], &min_max.min, sizeof(int));
+      read(pipefd[0], &min_max.max, sizeof(int));
     }
 
     if (min < min_max.min) min_max.min = min;
     if (max > min_max.max) min_max.max = max;
   }
-  fclose(fp);  // 1
+  //if(with_files) fclose(file);
 
   struct timeval finish_time;
   gettimeofday(&finish_time, NULL);
@@ -210,5 +222,17 @@ int main(int argc, char **argv) {
   printf("Max: %d\n", min_max.max);
   printf("Elapsed time: %fms\n", elapsed_time);
   fflush(NULL);
+
+
   return 0;
 }
+
+void kill_child(pid_t id){
+  kill(id, SIGKILL);
+  printf("child die\t");
+  int status;
+  wait(&status);
+}
+
+
+
