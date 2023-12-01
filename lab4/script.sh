979bffc5c5af035a959cad4960d62020b386da73
Aleks226 <83003755+Aleks226@users.noreply.github.com>
2023-12-01 05:11:07 +0000

    добавлен makefile

---
+++ lab4/script.sh
@@ -0,0 +1,25 @@
+clear
+cd lab4/src
+
+# 1
+echo "Задание №1-3"
+make all
+./parallel --seed "2" --array_size "8" --pnum "4" --timeout "10"
+make clean
+
+# 4
+echo "Задание №4"
+
+
+# 5
+echo "Задание №5"
+
+
+# end
+echo "Конец"
+cd ..
+git add ./src/makefile
+git add ./script.sh
+git commit -m "добавлен makefile"
+git push
+cd ..
